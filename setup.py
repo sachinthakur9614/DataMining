5a25803c3ef7c4cc5f775c4af1d55b691617c00a
sachinthakur9614 <45144620+sachinthakur9614@users.noreply.github.com>
2019-12-09 12:49:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Sachin Thakur',                   # Type in your name
   author_email = 'sachin.thakur@mca.christuniversity.in',      # Type in your E-Mail
   url = 'https://github.com/sachinthakur9614/bin_by_median.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ankushmundhra/min_max/archive/v0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sachinthakur9614/bin_by_median/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['binning method', 'bin_by_median', 'python','Data Mining'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
